fd449d161ac022b17ede27a0da01d36f215c3049
LAPTOP-SOURAV <1997mehra@gmail.com>
2020-05-05 18:39:32 +0530

    Created 2nd endpoint which accepts question ID and returns the accepted or top-voted results

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ app.get("/api/topquestions",(req,res) => {
             .then(response => {
                 res.send(response.data.items);
             })
+            .catch(error => res.send(error));
     }
     else
     {
@@ -54,9 +55,86 @@ app.get("/api/topquestions",(req,res) => {
             .then(response =>{
                     res.send(response.data.items);
             })
-            .catch(error => res.send(error))
+            .catch(error => res.send(error));
     }
 });
+
+app.get("/api/answer",(req,res) => {
+    var {questionid:questionID} = req.body;
+    // console.log("Question ID:",questionid);
+    if(questionID)
+    {
+        var finalRes = {};
+        axios.get(`${URL}/questions/${questionID}`,{
+                params:{
+                    filter:"withbody",
+                    site:"stackoverflow",
+                    key:process.env.SECRET_KEY
+                }
+            })
+            .then(response => {
+                // console.log("Question response:",response.data)
+                finalRes['question_body'] = response.data.items[0].body;
+                finalRes['has_accepted_answer_id'] = response.data.items[0].is_answered? true: false;
+
+                //If the question has an accepted answer, then return that answer's body
+                if(response.data.items[0].accepted_answer_id)
+                {
+                    var answerID = response.data.items[0].accepted_answer_id;
+                    axios.get(`${URL}/answers/${answerID}`,{
+                        params:{
+                            filter:"withbody",
+                            site:"stackoverflow",
+                            key:process.env.SECRET_KEY,
+                        }
+                    })
+                    .then(resp => {
+                        finalRes['answer_body'] = resp.data.items[0].body;
+                        // console.log(finalRes);
+                        res.send(finalRes);
+                    })
+                    .catch(error => res.send(error));
+                }
+                else //If the question has not been answered, then return all the answers ordered by maximum votes
+                {
+                    // var page = 1;
+                    // var has_more = true;
+                    var answerArr = [];
+                    
+                    axios.get(`${URL}/questions/${questionID}/answers/`,{
+                            params:{
+                                order:"desc",
+                                sort:"votes",
+                                // page:9,
+                                pagesize:100,
+                                filter:"withbody",
+                                site:"stackoverflow",
+                                key:process.env.SECRET_KEY,
+                            }
+                        })
+                        .then(resp => {
+                            // var answerArr = [];
+                            // has_more = resp.data.has_more;
+                            resp.data.items.forEach(item =>  answerArr.push(item.body));
+                            finalRes['answer_array'] = answerArr;
+                            // console.log(answerArr.length);
+                            res.send(finalRes);
+                        })
+                        .catch(error => res.send(error));
+                }
+            })
+            .catch(error => res.send(error));
+    }
+    else
+    {
+        res.send("No question ID provided");
+    }
+});
+
+
+
+
+
 
 
 
